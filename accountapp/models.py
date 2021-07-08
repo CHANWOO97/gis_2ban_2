496c95813fca6fe96214113e833603f55e83da96
rkaeh <rkaehd127@gmail.com>
2021-07-08 16:47:23 +0900

    7/8 2ban class commit_1

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
+## 터미널에서 : python manage.py makemigrations
 # Create your models here.
+class HelloWorld(models.Model): #모델스안에 대문자 모델 상속
+    text = models.CharField(max_length=255, null=False) # CharField 문자열(최대길이 255, 공백 조건)
