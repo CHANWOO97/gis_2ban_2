bedf9329c55904e41db90303d861f11a6164af07
rkaeh <rkaehd127@gmail.com>
2021-08-05 17:53:04 +0900

    08/05 2ban class commit_2~4 pagination.html detail.html list.html urls.py views.py

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,14 +1,16 @@
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
 
 from articleapp.decorators import article_ownership_required
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
+
 
 @method_decorator(login_required, 'get')
 @method_decorator(login_required, 'post')
@@ -23,6 +25,7 @@ class ArticleCreateView(CreateView):
 
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk': self.object.pk})  # self.object == target_article
+
 
 class ArticleDetailView(DetailView):
     model = Article
@@ -49,3 +52,10 @@ class ArticleDeleteView(DeleteView):
     context_object_name = 'target_article'
     success_url = reverse_lazy('articleapp:list')
     template_name = 'articleapp/delete.html'
+
+
+class ArticleListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'articleapp/list.html'
+    paginate_by = 1
