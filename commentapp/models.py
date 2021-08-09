c43276290265dd2d186fe2d3ce137f4ade25646f
rkaeh <rkaehd127@gmail.com>
2021-08-09 16:56:54 +0900

    08/09 2ban class commit_2 forms.py models.py <-> db makemigrations

--- commentapp/models.py
+++ commentapp/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from articleapp.models import Article
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.SET_NULL,
+                                related_name='comment', null=True)
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL,
+                               related_name='comment', null=True) # 장고에서 제공해주는 유저 임포트
+
+    content = models.TextField(null=False)
+
+    created_at = models.DateTimeField(auto_now_add=True) # db 에 저장되는 순간 타임~
