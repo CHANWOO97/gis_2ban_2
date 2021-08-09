f620266b23a260578d816390eaf4991ab80863aa
rkaeh <rkaehd127@gmail.com>
2021-08-09 19:30:08 +0900

    08/09 2ban class db profileapp id 오류 db commentapp 전체수정본

--- commentapp/models.py
+++ commentapp/models.py
@@ -3,14 +3,16 @@ from django.db import models
 
 # Create your models here.
 from articleapp.models import Article
-# ORM모델은 쿼리문 없이 장고에서 데이터베이스와 소통하기 위한 것이다.
+
 
 class Comment(models.Model):
+
     article = models.ForeignKey(Article, on_delete=models.SET_NULL,
                                 related_name='comment', null=True)
     writer = models.ForeignKey(User, on_delete=models.SET_NULL,
-                               related_name='comment', null=True) # 장고에서 제공해주는 유저 임포트
+                               related_name='comment', null=True)  # 장고에서 제공해주는 유저 임포트
 
     content = models.TextField(null=False)
 
-    created_at = models.DateTimeField(auto_now_add=True) # db 에 저장되는 순간 타임~
+    created_at = models.DateTimeField(auto_now_add=True)  # db 에 저장되는 순간 타임~
+
