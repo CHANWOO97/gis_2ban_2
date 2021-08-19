0ee2ec85915a0336028e8a13d1bf6251ba3d6885
rkaeh <rkaehd127@gmail.com>
2021-08-19 17:03:46 +0900

    08/19 2ban class commit_1

--- projectapp/models.py
+++ projectapp/models.py
@@ -9,3 +9,6 @@ class Project(models.Model):
     image = models.ImageField(upload_to='project/', null=False) # media 폴더에 이미지 업로드
 
     created_at = models.DateTimeField(auto_now_add=True) # 클라이언트에 승인 받지 않아도 자동 생성
+
+    def __str__(self): #내부 스페셜 메소드 중 하나 // 출력할때 어떤 값을 되돌려줄지 설정하는 함수
+        return self.name
--- projectapp/views.py
+++ projectapp/views.py
@@ -5,7 +5,9 @@ from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic.list import MultipleObjectMixin
 
+from articleapp.models import Article
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
 
@@ -21,10 +23,16 @@ class ProjectCreateView(CreateView):
         return reverse('projectapp:detail', kwargs={'pk': self.object.pk})
 
 
-class ProjectDetailView(DetailView):
+class ProjectDetailView(DetailView, MultipleObjectMixin): # listview를 같이 사용하는 거랑 다름없는 ,,
     model = Project
     context_object_name = 'target_project'
     template_name = 'projectapp/detail.html'
+
+    paginate_by = 20
+
+    def get_context_data(self, **kwargs): # 무슨 역할?
+        article_list = Article.objects.filter()
+        return super().get_context_data(**kwargs)
 
 class ProjectListView(ListView):
     model = Project
