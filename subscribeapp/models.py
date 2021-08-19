dba9dd4565303adac3a3491ebb040881aef07074
rkaeh <rkaehd127@gmail.com>
2021-08-19 18:02:32 +0900

    08/19 2ban class commit_5 subscribeapp makemigrations and migrate

--- subscribeapp/models.py
+++ subscribeapp/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='subscription', null=False)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE,
+                                related_name='subscription', null=False)
+
+    class Meta: # 외부정보 장고용어 meta
+        unique_together = ['user', 'project'] # unique 지정해줌
