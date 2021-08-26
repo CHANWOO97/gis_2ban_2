a2266ba95c064f348cfec6655643d806e8cd0065
rkaeh <rkaehd127@gmail.com>
2021-08-26 18:07:09 +0900

    08/26 2ban class commit_4
    header.html  span tag margin 2 success
    main urls.py -> path home add
    article -> models.py <blank add> migrate

--- articleapp/models.py
+++ articleapp/models.py
@@ -10,7 +10,7 @@ class Article(models.Model):
                                related_name='article', null=True)
 
     project = models.ForeignKey(Project, on_delete=models.SET_NULL,
-                               related_name='article', null=True)
+                               related_name='article', null=True, blank=True) # DB 에서만 NULL값 허용 , blank 사용시 이제 허용
 
     title = models.CharField(max_length=200, null=True) # 문자열 받을 수 있도록 쓰는 함수
     image = models.ImageField(upload_to='article/', null=True)
