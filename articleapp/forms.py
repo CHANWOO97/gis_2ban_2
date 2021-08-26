9d1f110a042070aea1d2dfcac601d333f1939daa
rkaeh <rkaehd127@gmail.com>
2021-08-26 16:57:46 +0900

    08/26 2ban class commit_1
    articleapp -> forms.py
    content add -> CharFiled-Textarea
    
    MediumEditor(web.form) -> create.html add
    
    detail => article_content -> html tag update -> | safe

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,9 +1,15 @@
+from django import forms
 from django.forms import ModelForm
 
 from articleapp.models import Article
 
 
 class ArticleCreationForm(ModelForm):
+    # 장고 공식문서에서 사용하는 문장
+    content = forms.CharField(widget=forms.Textarea(attrs={'class':'editable',
+                                                           'style':'min-height : 10rem;'
+                                                                    'text-align: left;'}))
+
     class Meta:
         model = Article
         fields = ['title', 'image', 'project', 'content']
